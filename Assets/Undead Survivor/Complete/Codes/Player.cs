cba15d202b2d9c3e695fad3c15759116c53c56e5
hyun044 <oie213059@gmail.com>
2024-11-14 04:12:02 +0900

    ddddddd
    d

--- Assets/Undead Survivor/Complete/Codes/Player.cs
+++ Assets/Undead Survivor/Complete/Codes/Player.cs
@@ -68,6 +68,9 @@ namespace Goldmetal.UndeadSurvivor
             if (!GameManager.instance.isLive)
                 return;
 
+            // 만약 몬스터가 아니면 return
+            // >> 상점의 구조물이나 상점의 벽면에 충돌했을 때 대미지를 입지 않기 위함
+
             GameManager.instance.health -= Time.deltaTime * 10;
 
             if (GameManager.instance.health < 0) {
@@ -79,6 +82,15 @@ namespace Goldmetal.UndeadSurvivor
                 GameManager.instance.GameOver();
             }
         }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (!GameManager.instance.isLive)
+                return;
+
+            // 만약 상점이라면 게임의 시간을 멈추고 씬을 상점으로 돌린다.
+        }
+
 
         void OnMove(InputValue value)
         {
--- Assets/Undead Survivor/Complete/Codes/StoreManager.cs
+++ Assets/Undead Survivor/Complete/Codes/StoreManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Store : MonoBehaviour
+public class StoreManager : MonoBehaviour
 {
     
+
 }
