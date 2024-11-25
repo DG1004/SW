4fb48b954fbb309a3c05f956b7c707414f9ffc3c
hyun044 <oie213059@gmail.com>
2024-11-26 06:10:50 +0900

    상점, 상점 화살표
    상점 스폰 알고리즘 추가
    상점으로의 화살표 추가

--- Assets/Undead Survivor/Complete/Codes/Player.cs
+++ Assets/Undead Survivor/Complete/Codes/Player.cs
@@ -6,142 +6,147 @@ using Cinemachine;
 
 namespace Goldmetal.UndeadSurvivor
 {
-    public class Player : MonoBehaviour
-    {
-        public Vector2 inputVec;
-        public float speed;
-        public int isStore;
-        Vector3 prePos;
-        public Scanner scanner;
-        public Hand[] hands;
-        public RuntimeAnimatorController[] animCon;
-        public Rigidbody2D rigid;
+	public class Player : MonoBehaviour
+	{
+		public Vector2 inputVec;
+		public float speed;
+		public int isStore;
+		Vector3 prePos;
+		public Scanner scanner;
+		public Hand[] hands;
+		public RuntimeAnimatorController[] animCon;
+		public Rigidbody2D rigid;
 
-        public GameObject store_inside;
-        public GameObject vcam;
-        public GameObject Pool;
+		public GameObject store_inside;
+		public GameObject vcam;
+		public GameObject Pool;
+		public Store store;
 
-        SpriteRenderer spriter;
-        Animator anim;
+		SpriteRenderer spriter;
+		Animator anim;
 
-        void Awake()
-        {
-            rigid = GetComponent<Rigidbody2D>();
-            spriter = GetComponent<SpriteRenderer>();
-            anim = GetComponent<Animator>();
-            scanner = GetComponent<Scanner>();
-            hands = GetComponentsInChildren<Hand>(true);
-        }
+		void Awake()
+		{
+			rigid = GetComponent<Rigidbody2D>();
+			spriter = GetComponent<SpriteRenderer>();
+			anim = GetComponent<Animator>();
+			scanner = GetComponent<Scanner>();
+			hands = GetComponentsInChildren<Hand>(true);
+		}
 
-        void OnEnable()
-        {
-            speed *= Character.Speed;
-            anim.runtimeAnimatorController = animCon[GameManager.instance.playerId];
-        }
+		void OnEnable()
+		{
+			speed *= Character.Speed;
+			anim.runtimeAnimatorController = animCon[GameManager.instance.playerId];
+		}
 
-        void Update()
-        {
-            if (!GameManager.instance.isLive)
-                return;
+		void Update()
+		{
+			if (!GameManager.instance.isLive)
+				return;
 
-            inputVec.x = Input.GetAxisRaw("Horizontal");
-            inputVec.y = Input.GetAxisRaw("Vertical");
-        }
+			inputVec.x = Input.GetAxisRaw("Horizontal");
+			inputVec.y = Input.GetAxisRaw("Vertical");
+		}
 
-        void FixedUpdate()
-        {
-            if (!GameManager.instance.isLive)
-                return;
+		void FixedUpdate()
+		{
+			if (!GameManager.instance.isLive)
+				return;
 
-            Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
-            rigid.MovePosition(rigid.position + nextVec);
-        }
+			Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+			rigid.MovePosition(rigid.position + nextVec);
+		}
 
-        void LateUpdate()
-        {
-            if (!GameManager.instance.isLive)
-                return;
+		void LateUpdate()
+		{
+			if (!GameManager.instance.isLive)
+				return;
 
-            anim.SetFloat("Speed", inputVec.magnitude);
+			anim.SetFloat("Speed", inputVec.magnitude);
 
 
-            if (inputVec.x != 0) {
-                spriter.flipX = inputVec.x < 0;
-            }
-        }
+			if (inputVec.x != 0) {
+				spriter.flipX = inputVec.x < 0;
+			}
+		}
 
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            // 플레이어가 상점 입구에 충돌했을 때
-            if (collision.gameObject.CompareTag("StoreEntrance"))
-            {
-                // 플레이어가 상점에 있다는 것을 표시합니다.
-                isStore = 1;
-                // 게임 진행 상태를 false로 설정합니다.
-                GameManager.instance.isLive = false;
-                // 플레이어의 기존 위치를 기억합니다.
-                prePos = transform.position;
-                // 플레이어의 위치를 상점으로 순간이동합니다.
-                transform.position = new Vector3(store_inside.transform.position.x, store_inside.transform.position.y, store_inside.transform.position.z);
-                // 카메라를 바꿉니다.
-                vcam.GetComponent<CinemachineVirtualCamera>().Priority = 11;
-            }
-            // 플레이어가 상점 출구에 충돌했을 때
-            if (collision.gameObject.CompareTag("StoreExit"))
-            {
-                isStore = 0;
-                transform.position = new Vector3(prePos.x + 1, prePos.y + 1, prePos.z + 1);
-                //wait();
-                GameManager.instance.isLive = true;
+		void OnCollisionEnter2D(Collision2D collision)
+		{
+			// 플레이어가 상점 입구에 충돌했을 때
+			if (collision.gameObject.CompareTag("StoreEntrance"))
+			{
+				// 플레이어가 상점에 있다는 것을 표시합니다.
+				isStore = 1;
+				// 게임 진행 상태를 false로 설정합니다.
+				GameManager.instance.isLive = false;
+				// 플레이어의 기존 위치를 기억합니다.
+				prePos = transform.position;
+				// 플레이어의 위치를 상점으로 순간이동합니다.
+				transform.position = new Vector3(store_inside.transform.position.x, store_inside.transform.position.y, store_inside.transform.position.z);
+				// 카메라를 바꿉니다.
+				vcam.GetComponent<CinemachineVirtualCamera>().Priority = 11;
+				store.gameObject.SetActive(false);
+			}
+			// 플레이어가 상점 출구에 충돌했을 때
+			if (collision.gameObject.CompareTag("StoreExit"))
+			{
+				isStore = 0;
+				transform.position = new Vector3(prePos.x + 1, prePos.y + 1, prePos.z + 1);
+				//wait();
+				GameManager.instance.isLive = true;
 
-                // 상점에서 나갈 때 카메라가 cut방식으로 바로 전환되지 않는 것 수정 필요
-                vcam.GetComponent<CinemachineVirtualCamera>().Priority = 9;
-            }
-            // 플레이어가 기본상점에 충돌했을 때
-            if (collision.gameObject.CompareTag("StoreStd"))
-            {
+				store.changePosition();
+				store.gameObject.SetActive(true);
 
-            }
-            // 플레이어가 대장장이에 충돌했을 때
-            if (collision.gameObject.CompareTag("StoreSmith"))
-            {
+				// 상점에서 나갈 때 카메라가 cut방식으로 바로 전환되지 않는 것 수정 필요
+				vcam.GetComponent<CinemachineVirtualCamera>().Priority = 9;
+			}
+			// 플레이어가 기본상점에 충돌했을 때
+			if (collision.gameObject.CompareTag("StoreStd"))
+			{
 
-            }
-        }
-        void OnCollisionStay2D(Collision2D collision)
-        {
-            if (!GameManager.instance.isLive)
-                return;
-            
-            if (collision.gameObject.CompareTag("travellingMerchant"))
-            {
-                GameManager.instance.ShowShop();
-            }
-            else if (collision.gameObject.CompareTag("Enemy"))
-            {
-                GameManager.instance.health -= Time.deltaTime * 10;
+			}
+			// 플레이어가 대장장이에 충돌했을 때
+			if (collision.gameObject.CompareTag("StoreSmith"))
+			{
 
-                if (GameManager.instance.health < 0)
-                {
-                    for (int index = 2; index < transform.childCount; index++)
-                    {
-                        transform.GetChild(index).gameObject.SetActive(false);
-                    }
+			}
+		}
+		void OnCollisionStay2D(Collision2D collision)
+		{
+			if (!GameManager.instance.isLive)
+				return;
+			
+			if (collision.gameObject.CompareTag("travellingMerchant"))
+			{
+				GameManager.instance.ShowShop();
+			}
+			else if (collision.gameObject.CompareTag("Enemy"))
+			{
+				GameManager.instance.health -= Time.deltaTime * 10;
 
-                    anim.SetTrigger("Dead");
-                    GameManager.instance.GameOver();
-                }
-            }
-        }
+				if (GameManager.instance.health < 0)
+				{
+					for (int index = 2; index < transform.childCount; index++)
+					{
+						transform.GetChild(index).gameObject.SetActive(false);
+					}
 
-        void OnMove(InputValue value)
-        {
-            inputVec = value.Get<Vector2>();
-        }
+					anim.SetTrigger("Dead");
+					GameManager.instance.GameOver();
+				}
+			}
+		}
 
-        IEnumerator wait()
-        {
-            yield return new WaitForSecondsRealtime(10);
-        }
-    }
+		void OnMove(InputValue value)
+		{
+			inputVec = value.Get<Vector2>();
+		}
+
+		IEnumerator wait()
+		{
+			yield return new WaitForSecondsRealtime(10);
+		}
+	}
 }
--- Assets/Undead Survivor/Complete/Codes/Store.cs
+++ Assets/Undead Survivor/Complete/Codes/Store.cs
@@ -4,9 +4,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using Random = UnityEngine.Random;
+
 
 public class Store : MonoBehaviour
 {
@@ -16,36 +19,90 @@ public class Store : MonoBehaviour
 	//
 	private void Update()
 	{
+		// 게임이 실행중일 때만 코드를 실행
+		if (GameManager.instance.isLive == false)
+			return;
+
+		// 일정시간이 지나면 상점의 위치를 바꾼다.
 		time += Time.deltaTime;
-		if (time > 9f)
+		if (time > 180f)
 		{
 			time = 0;
 			changePosition();
 		}
 	}
 
-	void changePosition()
+
+    public void changePosition()
 	{
+		// 상점의 위치
 		// x^2 + y^2 = distance^2
-		float distance = player.GetComponent<Player>().speed * Time.fixedDeltaTime * 900;
+		float distance = player.GetComponent<Player>().speed * Time.fixedDeltaTime * 200;
 		// x^2 < distance^2   ,   x < distance
 		float pos_x = Random.Range(0, distance);
 		// y^2 = distance^2 - x^2   ,   y = root(distance^2 - x^2)
 		float pos_y = Mathf.Sqrt(Mathf.Pow(distance, 2) - Mathf.Pow(pos_x, 2));
 
-		int maxX = 350;     //가로 350, 세로 250
-		int maxY = 250;
+		// 상점의 방향
 		/*
 		기본확률: 50%
 		벽과 가까워질수록 벽과 반대쪽의 방향이 될 확률이 높아짐(제곱을 활용)
-
 		 */
-		double dir_x = 0, dir_y = 0;
-		dir_x = Mathf.Pow(player.transform.position.x, 2) / Mathf.Pow(maxX, 2);
-		dir_y = Mathf.Pow(player.transform.position.y, 2) / Mathf.Pow(maxY, 2);
+		double weight_x = 0, weight_y = 0;
+		int ran_x, ran_y;
+        int maxX = 350;     //가로 350, 세로 250
+        int maxY = 250;
+        ran_x = Random.Range(0, 100);
+        ran_y = Random.Range(0, 100);
+
+		weight_x = MathF.Sign(player.transform.position.x) * Mathf.Pow(player.transform.position.x, 2) / Mathf.Pow(maxX, 2) * 100;
+		weight_y = MathF.Sign(player.transform.position.y) * Mathf.Pow(player.transform.position.y, 2) / Mathf.Pow(maxY, 2) * 100;
+
+		int dir_x=1;
+		int dir_y=1;
+
+        // 상점 위치가 ground 위가 아닐 경우 재호출 필요
+
+        if ((double)ran_x + weight_x >= 50 && (double)ran_y + weight_y >= 50)
+		{
+			// x반대, y반대방향
+			//transform.position = new Vector3(player.transform.position.x - pos_x, player.transform.position.y - pos_y, 0);
+			dir_x = -1;
+			dir_y = -1;
+            Debug.Log("--");
+		}
+		else if ((double)ran_x + weight_x < 50 && (double)ran_y + weight_y >= 50)
+		{
+			// x정, y반대방향
+			//transform.position = new Vector3(player.transform.position.x + pos_x, player.transform.position.y - pos_y, 0);
+			dir_x = 1;
+			dir_y = -1;
+            Debug.Log("+-");
+        }
+        else if ((double)ran_x + weight_x >= 50 && (double)ran_y + weight_y < 50)
+		{
+			// x반대, y정방향
+			//transform.position = new Vector3(player.transform.position.x - pos_x, player.transform.position.y + pos_y, 0);
+			dir_x = -1;
+			dir_y = 1;
+            Debug.Log("-+");
+
+        }
+        else if ((double)ran_x + weight_x < 50 && (double)ran_y + weight_y < 50)
+		{
+			// x정, y정방향
+			//transform.position = new Vector3(player.transform.position.x + pos_x, player.transform.position.y + pos_y, 0);
+			dir_x = 1;
+			dir_y = 1;
+            Debug.Log("++");
+        }
+		transform.position = new Vector3(player.transform.position.x + (dir_x * pos_x), player.transform.position.y + (dir_y * pos_y), 0);
+
+		if (transform.position.x >= maxX || transform.position.x <= -maxX || transform.position.y >= maxY || transform.position.y <= -maxY)
+		{
+			changePosition();
+		}
+    }
 
 
-        
-		Debug.Log(distance);
-	}
 }
