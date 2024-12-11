17ec90e1e93811e12fd7e2490f48da27de4c743d
DG1004 <ydh05300@gmail.com>
2024-12-11 23:42:13 +0900

    게임 시간 상승식으로 변경
    게임 시간을 상승식으로 변경했습니다

--- Assets/Undead Survivor/Complete/Codes/HUD.cs
+++ Assets/Undead Survivor/Complete/Codes/HUD.cs
@@ -29,7 +29,7 @@ namespace Goldmetal.UndeadSurvivor
                     myText.text = string.Format("{0:F0}", GameManager.instance.kill);
                     break;
                 case InfoType.Time:
-                    float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                    float remainTime = GameManager.instance.gameTime;
                     int min = Mathf.FloorToInt(remainTime / 60);
                     int sec = Mathf.FloorToInt(remainTime % 60);
                     myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
